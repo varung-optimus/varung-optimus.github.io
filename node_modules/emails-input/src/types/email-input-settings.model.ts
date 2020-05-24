1b72838e5df711ed55dd828ea9f2a3b1217dd948
Varun Goel <varungoel0806@gmail.com>
2020-05-24 14:17:19 +0200

    Adding demo page on git

---
+++ node_modules/emails-input/src/types/email-input-settings.model.ts
@@ -0,0 +1,12 @@
+export class EmailInputSettings {
+    // Functionality settings
+    isEnterEnabled?: boolean = true;
+    isCommaEnabled?: boolean = true;
+    isBlurEnabled?: boolean = true;
+    isSpaceEnabled?: boolean = true;
+    // Styling settings
+    textElementClasses?: string;
+    placeholder?: string;
+    // Data
+    domain?: string;
+}
---
+++ node_modules/emails-input/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader'
+                }]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebPackPlugin()
+    ]
+};
